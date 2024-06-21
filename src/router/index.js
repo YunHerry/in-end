ca8d75c7ea76da78cf85a8116c9054cf987481a1
云荷YunHerry <54676890+YunHerry@users.noreply.github.com>
2024-06-21 11:57:35 +0800

    add MyView.vue

--- src/router/index.js
+++ src/router/index.js
@@ -5,12 +5,14 @@ import EssayView from "../views/EssayView.vue";
 import ResourceView from "../views/ResourceIndexView.vue";
 import ResourceDetailView from "../views/ResourceDetailView.vue";
 import ProfileView from "../views/ProfileView.vue";
+import MyView from "../views/MyView.vue";
 const routes = [
   { path: "/", component: IndexView },
   { path: "/essay", component: EssayView },
   { path: "/resource", component: ResourceView },
   { path: "/resourceDetail", component: ResourceDetailView },
   { path: "/profile", component: ProfileView },
+  {path: "/my",component: MyView}
 ];
 const router = createRouter({
   history: createWebHashHistory(),
